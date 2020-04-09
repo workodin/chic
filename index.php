b78901462e26b227542cfb5ec3c6bbd355055441
test1 <test1@mail.me>
2020-04-09 20:28:13 +0200

    v.0.2.0 c++

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-
 <?php
 
-require_once "php/class/App.php";
-App::start();
 
--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 
 <?php
 
-require_once "../index.php";
+require_once __DIR__ . "/../php/class/App.php";
+App::start();
