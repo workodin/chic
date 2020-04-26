d560cc057e75fd39f1bd5c046754dde3b9d4117b
workodin <hello@workodin.com>
2020-04-26 20:30:24 +0200

    v0.5.5 c++

--- php/class/dev/Mime.php
+++ php/class/dev/Mime.php
@@ -23,29 +23,29 @@ class Mime
 
         $mime_types = array(
 
-            'txt' => 'text/plain',
-            'htm' => 'text/html',
-            'html' => 'text/html',
-            'php' => 'text/html',
-            'css' => 'text/css',
-            'js' => 'application/javascript',
-            'json' => 'application/json',
-            'xml' => 'application/xml',
-            'swf' => 'application/x-shockwave-flash',
-            'flv' => 'video/x-flv',
+            'txt'   => 'text/plain',
+            'htm'   => 'text/html',
+            'html'  => 'text/html',
+            'php'   => 'text/html',
+            'css'   => 'text/css',
+            'js'    => 'application/javascript',
+            'json'  => 'application/json',
+            'xml'   => 'application/xml',
+            'swf'   => 'application/x-shockwave-flash',
+            'flv'   => 'video/x-flv',
 
             // images
-            'png' => 'image/png',
-            'jpe' => 'image/jpeg',
-            'jpeg' => 'image/jpeg',
-            'jpg' => 'image/jpeg',
-            'gif' => 'image/gif',
-            'bmp' => 'image/bmp',
-            'ico' => 'image/vnd.microsoft.icon',
-            'tiff' => 'image/tiff',
-            'tif' => 'image/tiff',
-            'svg' => 'image/svg+xml',
-            'svgz' => 'image/svg+xml',
+            'png'   => 'image/png',
+            'jpe'   => 'image/jpeg',
+            'jpeg'  => 'image/jpeg',
+            'jpg'   => 'image/jpeg',
+            'gif'   => 'image/gif',
+            'bmp'   => 'image/bmp',
+            'ico'   => 'image/vnd.microsoft.icon',
+            'tiff'  => 'image/tiff',
+            'tif'   => 'image/tiff',
+            'svg'   => 'image/svg+xml',
+            'svgz'  => 'image/svg+xml',
 
             // archives
             'zip' => 'application/zip',
@@ -56,15 +56,15 @@ class Mime
 
             // audio/video
             'mp3' => 'audio/mpeg',
-            'qt' => 'video/quicktime',
+            'qt'  => 'video/quicktime',
             'mov' => 'video/quicktime',
 
             // adobe
             'pdf' => 'application/pdf',
             'psd' => 'image/vnd.adobe.photoshop',
-            'ai' => 'application/postscript',
+            'ai'  => 'application/postscript',
             'eps' => 'application/postscript',
-            'ps' => 'application/postscript',
+            'ps'  => 'application/postscript',
 
             // ms office
             'doc' => 'application/msword',
